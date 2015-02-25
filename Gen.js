d55ee2760c7ca0631a0b69afa488cbaf0cc1b2f6
Shubham SHARMA <shubham@sharma.fr>
2015-02-25 15:57:16 +0100

    finish the first part

--- Gen.js
+++ Gen.js
@@ -47,88 +47,81 @@ array1 = new Array(
 
 // Nouns
 array2 = new Array(
-	new Array("lâ€™E-commerce",	0)
-	new Array("les chaines",	1)
-	new Array("les archtectures",	1)
-	new Array("les systems",	0)
-	new Array("lâ€™E-Marketing",	0)
-	new Array("les applications",	1)
-	new Array("le contenu",	0)
-	new Array("la convergence",	0)
-	new Array("le meeting",	0)
-	new Array("lâ€™E-Business",	0)
-	new Array("les E-services",	1)
-	new Array("lexperience",	0)
-	new Array("les livrables",	1)
-	new Array("les actions",	1)
-	new Array("les insfrastructures",	1)
-	new Array("lâ€™initiative",	0)
-	new Array("le market",	0)
-	new Array("les modeles",	1)
-	new Array("le ROI",	0)
-	new Array("les synergies",	1)
-	new Array("le schema",	0)
-	new Array("la solution",	0)
-	new Array("les utilisateurs",	1)
-	new Array("les web services",	1)
-	new Array("le relationnel",	0)
-	new Array("les plateformes",	1)
-	new Array("les partenariats",	1)
-	new Array("lEco-system",	0)
-	new Array("les paradigms",	1)
-	new Array("les niches",	1)
-	new Array("le reseau",	0)
-	new Array("lâ€™intelligence",	0)
-	new Array("la generation",	0)
-	new Array("linterference",	0)
-	new Array("l'impact",	0)
-	new Array("la loi",	0)
-	new Array("le Big-Data",	0)
-	new Array("la soliditÃ©",	0)
-	new Array("un positionnement",	0)
+	new Array("e-commerce",	0),
+	new Array("chanels",	1),
+	new Array("archtectures",	1),
+	new Array("systems",	0),
+	new Array("E-Markting",	0),
+	new Array("applications",	1),
+	new Array("contenu",	0),
+	new Array("convergence",	0),
+	new Array("meeting",	0),
+	new Array("E-Business",	0),
+	new Array("E-services",	1),
+	new Array("Experience",	0),
+	new Array("Actions",	1),
+	new Array("Infrastructues",	1),
+	new Array("Initiatives",	0),
+	new Array("Market",	0),
+	new Array("Money",	1),
+	new Array("ROI",	0),
+	new Array("Synergies",	1),
+	new Array("Scheme",	0),
+	new Array("Solution",	0),
+	new Array("User",	1),
+	new Array("web services",	1),
+	new Array("Network",	0),
+	new Array("plateforms",	1),
+	new Array("Partnerships",	1),
+	new Array("Eco-system",	0),
+	new Array("paradigms",	1),
+	new Array("niches",	1),
+	new Array("Intelligence",	0),
+	new Array("Generation",	0),
+	new Array("Interference",	0),
+	new Array("Imnpact",	0),
+	new Array("Law",	0),
+	new Array("Big-Data",	0),
+	new Array("Positionning",	0)
 	);
 
 //Adjectivs
 array3 = new Array(
-	new Array("innovant",		"innovants"),	
-	new Array("novateur",		"novateurs"),
-	new Array("temps rÃ©el",		"temps rÃ©el"),
+	new Array("innovative",		"innovative"),	
+	new Array("Real-time",		"Real-time"),
 	new Array("24/365",			"24/365"),
 	new Array("24/7",			"24/7"),
 	new Array("B2B",			"B2B"),
 	new Array("B2C",			"B2C"),
 	new Array("back-end",		"back-end"),
-	new Array("collaboratif",	"collaboratifs"),
-	new Array("distribuÃ©",		"distribuÃ©s"),
-	new Array("virtualisÃ©",		"virtualisÃ©s"),
+	new Array("Collaborative",	"Collaborative"),
+	new Array("Ditributed",		"Ditributed"),
+	new Array("Virtualised",		"Virtualised"),
 	new Array("end-to-end",		"end-to-end"),
-	new Array("global",			"globaux"),
-	new Array("efficace",		"efficaces"),
-	new Array("prÃ©cieux",		"prÃ©cieux"),
-	new Array("granulaire",		"granulaires"),
-	new Array("intuitif",		"intuitifs"),
-	new Array("interactif",		"interactifs"),
+	new Array("global",			"Global"),
+	new Array("efiicient",		"efiicient"),
+	new Array("granular",		"granular"),
+	new Array("intuitive",		"intuitive"),
+	new Array("interactiv",		"interactiv"),
 	new Array("one-to-one",		"one-to-one"),
 	new Array("next-generation",	"next-generation"),
-	new Array("nouvelle-generation",	"nouvelle-generation"),
+	new Array("new-generation",	"new-generation"),
 	new Array("open-source",	"open-source"),
-	new Array("robuste",		"robuste"),
-	new Array("riche",			"riches"),
-	new Array("extensible",		"extensibles"),
+	new Array("reliable",		"reliable"),
+	new Array("rich",			"rich"),
+	new Array("Scalable",		"Scalable"),
 	new Array("plug-and-play",	"plug-and-play"),
-	new Array("neuf",			"neufs"),
 	new Array("sexy",			"sexy"),
-	new Array("nouveau",		"nouveaux"),
-	new Array("disjoint",		"disjoints"),
-	new Array("sans-fil",		"sans-fil"),
-	new Array("abstrait",		"abstraits"),
-	new Array("virtuel",		"virtuels"),
+	new Array("new",		"new"),
+	new Array("disjoint",		"disjoint"),
+	new Array("wireless",		"wireless"),
+	new Array("abstract",		"abstract"),
 	new Array("virale",			"virales"),
-	new Array("transparent",	"transparents"),
-	new Array("visionaire",		"visionnaires"),
-	new Array("stratÃ©gique",	"stratÃ©giques"),
-	new Array("subversif",		"subversifs"),
-	new Array("libre",	"libres")
+	new Array("transparent",	"transparent"),
+	new Array("visionairy",		"visionairie"),
+	new Array("strategic",	"strategic"),
+	new Array("subversif",		"subversif"),
+	new Array("free",	"free")
 	);
 
 
@@ -143,10 +136,10 @@ jQuery(document).ready(function(){
 		index3 = Math.round(Math.random() * array3.count());
 
 		if ((array2[index2][1] == 0)){
-			jQuery("#my_bullshit").text(array1[index1] + " " + array2[index2][0] + " " + array3[index3][0]);
+			jQuery("#my_bullshit").text(array1[index1] + " " + array3[index3][0] + " " + array2[index2][0]);
 		}
 		else if ((array2[index2][1] == 1)){
-			jQuery("#my_bullshit").text(array1[index1] + " " + array2[index2][0] + " " + array3[index3][3]);
+			jQuery("#my_bullshit").text(array1[index1] + " " + array3[index3][1] + " " + array2[index2][0]);
 		}
 	});
 });
